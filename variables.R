6ee9af1e2acb82f3c987f2f762f52317bc19902f
Samhitha Raj <samraj@umich.edu>
2016-01-05 11:24:00 -0500

    Adding commented header in variables.R

---
+++ variables.R
@@ -0,0 +1,16 @@
+# This is the document where I learn introductory R.
+x==1
+mass <- 47.5
+age <- 122
+mass <- mass*2.3
+age <- age-20
+xxage <- 122
+mass <- mass*2.3
+age <- age-20
+ 
+y<- 2
+z<- y^2 #noo
+x+y+z
+x<- 1:10 
+
+
